33f661171b974ee1cab3180a9f6b79fec59c8ca7
FerozShaik96 <ferozshaik1997@gmail.com>
2023-05-23 16:30:58 +0530

    Implementing useContext overall the Project

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Card from "../UI/Card/Card";
+import AuthContext from "../../store/AuthContext/auth-context";
+import classes from "./Home.module.css";
+import Button from "../UI/Button/Button";
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
-
-const Home = (props) => {
+const Home = () => {
+  const authctx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={authctx.onLogout}>Logout</Button>
     </Card>
   );
 };
